033ac2b7f03c33c8a544aea5f90ee0898c3c7f6b
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-09 14:36:21 +0900

    if

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
     <h1>Calendar</h1>
-    <p>events:</p>
-    <p>{{ events }}</p>
-    <button type="submit">fetchEvents</button>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+    <CalendarDetails />
   </div>
 </template>
 
 <script>
+// axiosをインポートする
+import axios from 'axios';
+import CalendarDetails from './CalendarDetails.vue';
+
 export default {
   name: 'Calendar',
+  components: {
+    CalendarDetails,
+  },
   data: () => ({
     events: []
   }),
+  methods: {
+    fetchEvents() {
+      //GETリクエストを送信し、取得データをevents変数に代入する
+      axios
+        .get('http://localhost:3000/events')
+        .then(response => {
+          this.events = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
 };
 </script>
---
+++ src/components/CarendarDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>CalendarDetails</h1>
+    <div v-for="event in events" :key="event.id">
+      <h2>{{ event.name }}</h2>
+      <p>start: {{ event.start }}</p>
+      <p>end: {{ event.end }}</p>
+      <p>description: {{ event.description }}</p>
+    </div>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarDetails',
+  data: () => ({
+    events: [],
+  }),
+  methods: {
+    fetchEvents() {
+      axios
+        .get('http://localhost:3000/events')
+        .then((response) => {
+          this.events = response.date;
+        })
+        .catch((error) => {
+          console.error(error)
+        });
+    },
+  },
+};
+</script>
