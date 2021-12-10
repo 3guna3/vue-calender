8263fbb5c7c7993c612d8fa63fbd1cf99e077ab1
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-10 16:11:01 +0900

    DialogSectionコンポーネントを作成

---
+++ src/components/DialogSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col col="2" clas="d-flex justify-center align-center">
+      <v-ion size="20px" :color="color">{{ icon }}</v-ion>
+    </v-col>
+    <v-col class="d-flex align-center">
+      <slot></slot>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'DialogSection',
+  props: ['icon', 'color'],
+};
+</script>
