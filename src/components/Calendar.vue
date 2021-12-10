fe7a432ac79b3b1cd6a2bbb40ebf2d7144a20402
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-10 16:03:28 +0900

    EventDetailDialogコンポーネントを作成し、ファイルを分離

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -32,43 +32,7 @@
     予定をクリックした時にdialogMessageに値が代入されてダイアログが表示される-->
     <!--@click:outsideというのはダイアログの外側をクリックした際のイベント発火-->
     <v-dialog :value="event !== null" @click:outside="closeDialog" width="600">
-      <div v-if="event !== null">
-        <v-card class="pb-12">
-          <v-card-actions class="d-flex justify-end pa-2">
-            <v-btn icon @click="closeDialog">
-              <v-icon size="20px">mdi-close</v-icon>
-            </v-btn>
-          </v-card-actions>
-          <v-card-title>
-            <v-row>
-              <v-col cols="2" class="d-flex justify-center align-center">
-                <v-icon size="20px" :color="event.color || 'blue'">mdi-square</v-icon>
-              </v-col>
-              <v-col class="d-flex align-center">
-                {{ event.name }}
-              </v-col>
-            </v-row>
-          </v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="2" class="d-flex justify-center align-center">
-                <v-icon size="20px">mdi-clock-time-three-outline</v-icon>
-              </v-col>
-              <v-col class="d-flex align-center"> {{ event.start.toLocaleString() }} ~ {{ event.end.toLocaleString() }} </v-col>
-            </v-row>
-          </v-card-text>
-          <v-card-text>
-            <v-row>
-              <v-col col="2" class="d-flex justify-center align-center">
-                <v-icon size="20px">mdi-card-text-outline</v-icon>
-              </v-col>
-              <v-col class="d-flex align-center">
-                {{ event.description || 'no description' }}
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </div>
+      <EventDetailDialog v-if="event !== null" />
     </v-dialog>
   </div>
 </template>
@@ -76,9 +40,13 @@
 <script>
 import { format } from 'date-fns';
 import { mapGetters, mapActions } from 'vuex';
+import EventDetailDialog from './EventDetailDialog';
 
 export default {
   name: 'Calendar',
+  components: {
+    EventDetailDialog,
+  },
   data: () => ({
     // 表示する月を指定
     value: format(new Date(), 'yyyy/MM/dd'),
