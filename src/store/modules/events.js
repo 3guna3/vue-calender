81ea5dfc2530256053873882674f6d1b915c63e8
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-10 17:45:00 +0900

    isEditMode変数を作成

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -4,52 +4,58 @@ const apiUrl = 'http://localhost:3000';
 
 // 状態管理するデータを定義する
 const state = {
-  events: [],
-  event: null,
+    events: [],
+    event: null,
+    isEditMode: false,
 };
 
 // stateの値を取り出す関数を定義する
 const getters = {
-  events: (state) =>
-    state.events.map((event) => {
-      return {
-        ...event,
-        start: new Date(event.start),
-        end: new Date(event.end),
-      };
-    }),
-  event: (state) =>
-    state.event
-      ? {
-          ...state.event,
-          start: new Date(state.event.start),
-          end: new Date(state.event.end),
-        }
-      : null,
+    events: (state) =>
+        state.events.map((event) => {
+            return {
+                ...event,
+                start: new Date(event.start),
+                end: new Date(event.end),
+            };
+        }),
+    event: (state) =>
+        state.event ?
+        {
+            ...state.event,
+            start: new Date(state.event.start),
+            end: new Date(state.event.end),
+        } :
+        null,
+    isEditMode: (state) => state.isEditMode,
 };
 
 // eventsデータをstateに保存する関数を定義する
 const mutations = {
-  setEvents: (state, events) => (state.events = events),
-  setEvent: (state, event) => (state.event = event),
+    setEvents: (state, events) => (state.events = events),
+    setEvent: (state, event) => (state.event = event),
+    setEventMode: (state, bool) => (state.isEditMode = bool),
 };
 
 // axiosでAPIリクエストを送信してeventsデータを取得し、mutationを呼び出す関数を定義する
 const actions = {
-  async fetchEvents({ commit }) {
-    const response = await axios.get(`${apiUrl}/events`);
-    commit('setEvents', response.data); // mutationを呼び出す
-  },
-  // setEventアクションはsetEventミューテーションを呼び出すだけで、APIリクエストを送るようなことはしていない
-  setEvent({ commit }, event) {
-    commit('setEvent', event);
-  },
+    async fetchEvents({ commit }) {
+        const response = await axios.get(`${apiUrl}/events`);
+        commit('setEvents', response.data); // mutationを呼び出す
+    },
+    // setEventアクションはsetEventミューテーションを呼び出すだけで、APIリクエストを送るようなことはしていない
+    setEvent({ commit }, event) {
+        commit('setEvent', event);
+    },
+    setEditMode({ commit }, bool) {
+        commit('setEditMode', bool);
+    },
 };
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  mutations,
-  actions,
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions,
 };
