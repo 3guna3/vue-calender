bb09e4ccd6f950df957ae9ab67c0d2c2abae179d
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-10 12:11:52 +0900

    今日の月に戻るボタンを追加

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <v-sheet height="6vh" class="d-flex align-center">
-      <v-btn icon>
+      <!--今日の月に戻るボタン-->
+      <v-btn outlined small class="ma-4" @click="setToday">今日</v-btn>
+      <!--prev()メソッドはVuetifyのカレンダーコンポーネントで定義されているメソッド-->
+      <v-btn icon @click="$refs.calendar.prev()">
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
-      <v-btn icon>
+      <!--next()メソッドはVuetifyのカレンダーコンポーネントで定義されているメソッド-->
+      <v-btn icon @click="$refs.calendar.next()">
         <v-icon>mdi-chevron-right</v-icon>
       </v-btn>
       <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-sheet>
     <v-sheet height="94vh">
-      <v-calendar v-model="value" :events="events" @change="fetchEvents"></v-calendar>
+      <!--ここにref="calendar"とすることで、v-btnでv-calendarで定義されているメソッドを呼び出せる-->
+      <v-calendar ref="calendar" v-model="value" :events="events" @change="fetchEvents"></v-calendar>
     </v-sheet>
   </div>
 </template>
@@ -23,18 +28,22 @@ export default {
   name: 'Calendar',
   data: () => ({
     // 表示する月を指定
-    value: new Date('2021/07/01'),
+    value: format(new Date(), 'yyyy/MM/dd'),
   }),
   // stateに保存された値をmapGettersでimportしたeventsゲッターで取得し、ビューに表示します。
   computed: {
     ...mapGetters('events', ['events']),
     title() {
-      return format(this.value, 'yyyy年 M月');
+      return format(new Date(this.value), 'yyyy年 M月');
     },
   },
   // ボタンを押すと、mapActionsでimportしたストアのfetchEventsアクションが実行されてデータを取得し、stateに保存されます。
   methods: {
     ...mapActions('events', ['fetchEvents']),
+    // value変数に現在に日にちを代入するメソッド
+    setToday() {
+      this.value = format(new Date(),'yyyy/MM/dd')
+    },
   },
 };
 </script>
