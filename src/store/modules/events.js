174a34b9284a65bb9642ad4248e6234a3e8d18b1
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-10 13:45:14 +0900

    クリックした予定のデータをストアで管理する仕組みの作成

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -4,38 +4,52 @@ const apiUrl = 'http://localhost:3000';
 
 // 状態管理するデータを定義する
 const state = {
-  events: [],
+    events: [],
+    event: null,
 };
 
 // stateの値を取り出す関数を定義する
 const getters = {
-  events: (state) =>
-    state.events.map((event) => {
-      return {
-        ...event,
-        start: new Date(event.start),
-        end: new Date(event.end),
-      };
-    }),
+    events: (state) =>
+        state.events.map((event) => {
+            return {
+                ...event,
+                start: new Date(event.start),
+                end: new Date(event.end),
+            };
+        }),
+    event: (state) =>
+        state.event ?
+        {
+            ...state.event,
+            start: new Date(state.event.start),
+            end: new Date(state.event.end),
+        } :
+        null,
 };
 
 // eventsデータをstateに保存する関数を定義する
 const mutations = {
-  setEvents: (state, events) => (state.events = events),
+    setEvents: (state, events) => (state.events = events),
+    setEvent: (state, event) => (state.event = event),
 };
 
 // axiosでAPIリクエストを送信してeventsデータを取得し、mutationを呼び出す関数を定義する
 const actions = {
-  async fetchEvents({ commit }) {
-    const response = await axios.get(`${apiUrl}/events`);
-    commit('setEvents', response.data); // mutationを呼び出す
-  },
+    async fetchEvents({ commit }) {
+        const response = await axios.get(`${apiUrl}/events`);
+        commit('setEvents', response.data); // mutationを呼び出す
+    },
+    // setEventアクションはsetEventミューテーションを呼び出すだけで、APIリクエストを送るようなことはしていない
+    setEvent({ commit }, event) {
+        commit('setEvent', event);
+    },
 };
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  mutations,
-  actions,
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions,
 };
