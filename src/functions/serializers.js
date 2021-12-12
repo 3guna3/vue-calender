aaf33362616257f4f1bea61fe1e6df5925fbf184
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-12 10:13:21 +0900

    シリアライザを作成し、ストアの内容をリファクタリング

---
+++ src/functions/serializers.js
@@ -0,0 +1,17 @@
+import { format } from 'date-fns';
+
+// serializeEventメソッドは、ストアのゲッターで行っていた処理と同じように、startDateおよびendDateを追加したeventオブジェクトを返す。
+export const serializeEvent = (event) => {
+  if (event === null) {
+    return null;
+  }
+  const start = new Date(event.start);
+  const end = new Date(event.end);
+  return {
+    ...event,
+    start,
+    end,
+    startDate: format(start, 'yyyy/MM/dd'),
+    endDate: format(end, 'yyyy/MM/dd'),
+  };
+};
