b6f878d6a8d3f1439023ceb31ca25c7fcc8c98e7
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-12 16:47:48 +0900

    serializers.jsに日付を時間の文字列に変更する処理を追加

--- src/functions/serializers.js
+++ src/functions/serializers.js
@@ -2,17 +2,19 @@ import { format } from 'date-fns';
 
 // serializeEventメソッドは、ストアのゲッターで行っていた処理と同じように、startDateおよびendDateを追加したeventオブジェクトを返す。
 export const serializeEvent = (event) => {
-  if (event === null) {
-    return null;
-  }
-  const start = new Date(event.start);
-  const end = new Date(event.end);
-  return {
-    ...event,
-    start,
-    end,
-    startDate: format(start, 'yyyy/MM/dd'),
-    endDate: format(end, 'yyyy/MM/dd'),
-    color: event.color || '#2196F3',
-  };
+    if (event === null) {
+        return null;
+    }
+    const start = new Date(event.start);
+    const end = new Date(event.end);
+    return {
+        ...event,
+        start,
+        end,
+        startDate: format(start, 'yyyy/MM/dd'),
+        startTime: format(start, 'HH:mm'),
+        endDate: format(end, 'yyyy/MM/dd'),
+        endTime: format(end, 'HH:mm'),
+        color: event.color || '#2196F3',
+    };
 };
