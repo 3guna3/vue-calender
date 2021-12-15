609f88f3d7867aedef4c40d0de50c569cbf435cd
3guna3 <83042016+3guna3@users.noreply.github.com>
2021-12-16 08:56:23 +0900

    処理を分離

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { isDateWithinInterval } from '../../functions/datetime';
+import { isDateWithinInterval, compareDates } from '../../functions/datetime';
 import { serializeEvent } from '../../functions/serializers';
 
 const apiUrl = 'http://localhost:3000';
@@ -20,11 +20,7 @@ const getters = {
     state.events
       .map((event) => serializeEvent(event))
       .filter((event) => isDateWithinInterval(state.clickedDate, event.startDate, event.endDate))
-      .sort((a, b) => {
-        if (a.start < b.start) return -1;
-        if (a.start > b.start) return 1;
-        return 0;
-      }),
+      .sort(compareDates),
   isEditMode: (state) => state.isEditMode,
   clickedDate: (state) => state.clickedDate,
 };
